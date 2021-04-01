f4ed32610c851c6397efb12f0d2ae71d5f28f2ac
jonathan <jhector39@gmail.com>
2021-04-01 11:15:46 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = `JONATHAN MARI`;
+const firstInitial = currentUser[0];
+const restofName = currentUser.slice(1);
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, `+ currentUser + `!`;
+
+const shortGreeting = `Welcome, `+ firstInitial + `!`;
